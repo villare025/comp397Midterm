809faba75eef5e612c4e242f56f9cad67c57fa43
villare025 <empvillare@yahoo.com>
2016-10-18 21:32:08 -0400

    Add crosshair mouse

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -15,9 +15,16 @@ var scenes;
             // Create BG for scene and add to Game Scene container
             this._bg = new createjs.Bitmap(assets.getResult("BGGame"));
             this.addChild(this._bg);
+            // Mousy mouse
+            stage.cursor = "none"; // hide their mouse
+            this.myCursor = new createjs.Bitmap(assets.getResult("Mouse")); //use my mouse
+            this.addChild(this.myCursor); //add my mouse
             stage.addChild(this);
         };
         Play.prototype.update = function () {
+            // update position of my mouse 
+            this.myCursor.x = stage.mouseX + -35;
+            this.myCursor.y = stage.mouseY + -32;
         };
         Play.prototype._onEnemyClick = function (event) {
         };
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -2,24 +2,35 @@ module scenes {
     export class Play extends objects.Scene {
 
         private _bg: createjs.Bitmap;
+        private myCursor: createjs.Bitmap;
+
+        private _enemy: objects.Enemy;
 
         constructor() {
             super();
             this.start();
         }
 
-        public start() : void {
-             // Create BG for scene and add to Game Scene container
+        public start(): void {
+            // Create BG for scene and add to Game Scene container
             this._bg = new createjs.Bitmap(assets.getResult("BGGame"));
             this.addChild(this._bg);
+
+            // Mousy mouse
+            stage.cursor = "none"; // hide their mouse
+            this.myCursor = new createjs.Bitmap(assets.getResult("Mouse")); //use my mouse
+            this.addChild(this.myCursor); //add my mouse
 
             stage.addChild(this);
         }
 
-        public update() : void {
+        public update(): void {
+            // update position of my mouse 
+            this.myCursor.x = stage.mouseX + -35;
+            this.myCursor.y = stage.mouseY + -32;
         }
 
-        private _onEnemyClick(event : createjs.MouseEvent) : void {
+        private _onEnemyClick(event: createjs.MouseEvent): void {
         }
     }
 }
